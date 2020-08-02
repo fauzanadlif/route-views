a8e0ac5ff678d01268c1dc45569aefad5029d0d7
fauzanadlif <55770811+fauzanadlif@users.noreply.github.com>
2020-08-02 19:11:08 +0700

    Add files via upload

---
+++ myblog/home.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Profil</title>
+
+</head>
+<body>
+
+  <style>
+
+      .jumbotron{
+          background: url(screen-0.jpg) center center;
+      }
+      .tulisan{
+        font-size: 20px;
+        font-family: Roboto;
+      }
+      .display-5{
+        color: black;
+        font-size: 20px;
+        font-family: Roboto;
+      }
+      .navbar-nav{
+        font-family: Roboto;
+        font-weight: bold;
+        
+      }
+      
+  </style>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="navbar-nav text-white" style="font-size: 20px;"> 
+  <a class="nav-item nav-link" href="home">HOME <span class="sr-only">(current)</span></a>
+    <a class="nav-item nav-link" href="#profil">PROFIL</a>
+      <a class="nav-item nav-link" href="experience">EXPERIENCE</a>
+        <a class="nav-item nav-link" href="portofolio">PORTOFOLIO</a>
+      </div>
+    </div>
+</nav>
+
+  <div class="jumbotron">
+    <center>
+      <img src="my.jpeg" width="200px">
+      <h1></h1>
+        <h1 class="display-5" >Fauzan Adli Firdaus</h1>
+      <p class="display-5">
+          (Siswa)
+      </p>
+  <p class="display-5">SMK Taruna Bhakti Depok</p>
+    </center>
+  </div>
+
+
+ <div class="container">
+ <div class="row">
+    <div class="col text-center">
+      <h1 id="profil" style="font-family: Roboto">PROFILE</h1><br>
+    </div>
+  </div>
+    <p class="tulisan">
+    Nama saya Fauzan adli. Saya lahir di Depok, 28 Oktober 2002, usia 17 tahun. 
+    Saat ini saya bersekolah di SMK Taruna Bhakti Depok, dan saya sekarang berada di kelas 12 jurusan RPL (Rekayasa Perangkat Lunak). Dan berikut adalah data diri lengkap saya.
+    <br><br>
+
+<!-- Biodata -->
+        Nama : Fauzan adli firdaus
+        <br>
+        Kelas : XII RPL 1
+        <br>
+        Sekolah : SMK Taruna Bhakti Depok
+        <br>
+        Tempat, Tanggal Lahir : Depok, 28 Oktober 2002
+        <br>
+        Alamat : Jl.let.Hatta Sukirman rt03/02
+        <br>
+        Agama : Islam
+        <br>
+        Jenis Kelamin : laki-laki
+    <br><br><br>
+    </p>
+  </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
